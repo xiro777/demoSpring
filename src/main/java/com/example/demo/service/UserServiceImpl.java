421a4587aa7c6204fc0fd445b6859da5c9264e9b
Kacper Morawski <kacper.morawski98@gmail.com>
2022-05-26 11:30:57 +0200

    overriding userdetails

--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -37,6 +37,15 @@ public class UserServiceImpl implements UserService{
         return userRepository.save(user);
     }
 
-   
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if(user == null)
+        {
+            throw new UsernameNotFoundException("Invalid username or password");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(),mapRolesToAuthorities(user.getRoles()));
     }
+
+    
 }
