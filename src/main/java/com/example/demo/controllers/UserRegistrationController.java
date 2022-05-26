0284542a213cfc8aa17cdf345989e5d797917bd6
Kacper Morawski <kacper.morawski98@gmail.com>
2022-05-26 11:33:48 +0200

    Creating user registration controller

---
+++ src/main/java/com/example/demo/controllers/UserRegistrationController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controllers;
+
+import com.example.demo.service.UserService;
+import com.example.demo.web.UserRegistrationDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/registration")
+public class UserRegistrationController {
+    private UserService userService;
+
+    public UserRegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    
+    }
+}
