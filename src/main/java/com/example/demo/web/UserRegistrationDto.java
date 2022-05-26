a4fbfc6a325a55eafd19b1f46ecf1175a8c02a7b
Kacper Morawski <kacper.morawski98@gmail.com>
2022-05-26 11:35:33 +0200

    Creating class to get data from database

---
+++ src/main/java/com/example/demo/web/UserRegistrationDto.java
@@ -0,0 +1,25 @@
+package com.example.demo.web;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class UserRegistrationDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public UserRegistrationDto(){
+
+    }
+
+    public UserRegistrationDto(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+}
